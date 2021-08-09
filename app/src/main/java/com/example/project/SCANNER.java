43604a7bfd30acbdb5373628b5dcb6b869d520ec
Jayur <pjayur14@gmail.com>
2021-08-09 15:37:29 +0530

    Intial Commmit

--- app/src/main/java/com/example/project/SCANNER.java
+++ app/src/main/java/com/example/project/SCANNER.java
@@ -15,6 +15,8 @@ import android.provider.MediaStore;
 public class SCANNER extends AppCompatActivity {
 
     String permission_camera[] = {"android.permission.CAMERA"};
+    String permission_read_external[] = {"android.permission.READ_EXTERNAL_STORAGE"};
+    String permission_write_eternal[] = {"android.permission.WRITE_EXTERNAL_STORAGE"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,24 @@ public class SCANNER extends AppCompatActivity {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED)
         {
          ActivityCompat.requestPermissions(this, permission_camera, 20);
+        }
+        else
+        {
+
+        }
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+        {
+            ActivityCompat.requestPermissions(this, permission_read_external, 21);
+        }
+        else
+        {
+
+        }
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+        {
+            ActivityCompat.requestPermissions(this, permission_write_eternal, 22);
         }
         else
         {
